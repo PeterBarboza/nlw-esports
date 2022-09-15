294b3b357775ad4e01546197428b4b03304f8059
PeterBarboza <pedro.barboza.dev@gmail.com>
2022-09-15 13:04:33 -0300

    feat: First React Native class finished

---
+++ mobile/App.tsx
@@ -0,0 +1,33 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button({ title }: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello NLW!</Text>
+      <Button title="Send photo" />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
